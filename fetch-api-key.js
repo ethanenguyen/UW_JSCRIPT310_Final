882d38614f7eb3676714195d0adc93880fdbd841
Nguyen (US), Hai C <Hai.C.Nguyen@boeing.com>
2024-12-11 21:04:02 -0800

    Added fetch-api-key

---
+++ fetch-api-key.js
@@ -0,0 +1,15 @@
+
+
+const uatToken = 'abcad';
+const hostname = 'abcde.com';
+const path = '/bcai-public-api/conversation';
+const model = 'gpt-35-turbo';
+const source = 'api-system-identifier';
+
+module.exports = {
+    uatToken,
+    hostname,
+    path,
+    model,
+    source
+};
